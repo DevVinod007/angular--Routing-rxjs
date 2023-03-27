29bc4404cb18721f046ab7294f45449856b7eecc
Vinod <vinod8797@gmail.com>
2023-03-27 16:16:41 +0530

    all changes final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { AuthService } from './services/auth.service';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit ,OnDestroy {
+  userAdded:boolean = false;
   title = 'routing';
-
-  constructor(private authService:AuthService){
+  unSub! :Subscription
+  constructor(private authService:AuthService,private UserService:UserService){
 
   }
 
+  ngOnInit(): void {
+  this.unSub =  this.UserService.userAddedEvent.subscribe((data) => {
+      console.log(data)
+      this.userAdded = data;
+    })
+  }
 
   onLoginClick(){
    this.authService.login()
@@ -20,5 +29,9 @@ export class AppComponent {
 
   onLogoutClick(){
    this.authService.logOut()
+  }
+
+  ngOnDestroy(): void {
+    this.unSub.unsubscribe()
   }
 }
